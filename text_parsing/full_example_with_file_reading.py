fb9b7761ad5a5b302ecca966a87ea187f084904b
Mohamed Ewees <engmohamedewees@gmail.com>
2026-06-15 09:12:40 +0300

    full_example_parsing

--- text_parsing/full_example_with_file_reading.py
+++ text_parsing/full_example_with_file_reading.py
@@ -28,7 +28,7 @@ def chunk_with_overlap(text,chunk_size=200,overlap = 50):
 
 # 2.c Sentence based Chunking
 def sentence_chunking(text, max_sentences = 3): 
-    sentences = re.split(r'(?<=[.!?]\s+)', text.strip())
+    sentences = re.split(r'(?<=[.!?])\s+', text.strip())
     chunks = []
     for i in range(0,len(sentences),max_sentences):
         chunk = ' '.join(sentences[i: i + max_sentences])
@@ -37,7 +37,7 @@ def sentence_chunking(text, max_sentences = 3):
 
 # 2.d Paragraoh chunking
 def paragraph_chunk(text):
-    paragraph = re.split(r'\n\s*\n', text.split())
+    paragraph = re.split(r'\n\s*\n', text.strip())
     return [p.strip() for p in paragraph if p.strip()]
 
 ############################################################################################################################
@@ -48,4 +48,21 @@ def print_chunks(label,chunks):
     print(f"\n{'='*50}")
     for i,chunk in enumerate(chunks,1):
         print(f"\n[chunk {i}]\n{chunk}")
+#############################################################################################################################
 
+# 4. Main Function
+def main():
+    filepath = r'D:\programming\VS-Code_Workspace\RAG-Course\text_parsing\Sample_text.txt'
+    
+    # Step 1: Read the file
+    text = read_file(filepath)
+    print(f"File Loaded -- {len(text)} Characters\n")
+
+    # Step 2: Run all 4 chuniking methods
+    print_chunks("Fixed Size", fixed_size_chunking(text,chunk_size=200))
+    print_chunks("Fixed + Overlap", chunk_with_overlap(text,chunk_size=200,overlap=50))
+    print_chunks("Sentence_Based", sentence_chunking(text, max_sentences=3))
+    print_chunks("Paragraph-Based", paragraph_chunk(text))
+
+if __name__ == "__main__":
+    main()
